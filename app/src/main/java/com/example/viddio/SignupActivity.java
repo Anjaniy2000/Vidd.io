e8fa204b980a6057cd7eae8ed9dec26b9c494e57
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-02-22 23:29:08 +0530

    forth commit

---
+++ app/src/main/java/com/example/viddio/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.viddio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
