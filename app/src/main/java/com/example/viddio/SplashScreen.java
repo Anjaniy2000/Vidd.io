492ba2823075b8508b687fd48bca11c37741513a
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-02-22 22:30:31 +0530

    first commit

---
+++ app/src/main/java/com/example/viddio/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.example.viddio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        splashScreen_Task();
+    }
+
+    /* Working Of Splash Screen: */
+    private void splashScreen_Task() {
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
