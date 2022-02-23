68235a88b8650ac8ca5de201dc8ba802e3c3b5ef
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-02-23 13:59:52 +0530

    eleventh commit

--- app/src/main/java/com/example/viddio/MainActivity.java
+++ app/src/main/java/com/example/viddio/MainActivity.java
@@ -3,17 +3,59 @@ package com.example.viddio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.viddio.databinding.ActivityMainBinding;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private URL serverURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        try {
+            serverURL = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions =
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setServerURL(serverURL)
+                            .setWelcomePageEnabled(false)
+                            .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        binding.joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(binding.meetingCode.getText().toString())
+                        .setWelcomePageEnabled(false)
+                        .build();
+
+                JitsiMeetActivity.launch(MainActivity.this, options);
+
+            }
+        });
+
+        binding.shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
