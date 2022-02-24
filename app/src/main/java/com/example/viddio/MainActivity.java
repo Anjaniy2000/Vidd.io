c43a7c16e51dca7730c1e5c96ec0d788e8bdbadc
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-02-24 13:54:28 +0530

    sixteenth commit

--- app/src/main/java/com/example/viddio/MainActivity.java
+++ app/src/main/java/com/example/viddio/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.viddio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.viddio.databinding.ActivityMainBinding;
+import com.example.viddio.fragments.AboutFragment;
+import com.example.viddio.fragments.HomeFragment;
+import com.example.viddio.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -17,45 +25,77 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    private URL serverURL;
+//    private URL serverURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        try {
-            serverURL = new URL("https://meet.jit.si");
-            JitsiMeetConferenceOptions defaultOptions =
-                    new JitsiMeetConferenceOptions.Builder()
-                            .setServerURL(serverURL)
-                            .setWelcomePageEnabled(false)
-                            .build();
-            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(listener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeFragment()).commit();
 
 
-        binding.joinButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                        .setRoom(binding.meetingCode.getText().toString())
-                        .setWelcomePageEnabled(false)
-                        .build();
 
-                JitsiMeetActivity.launch(MainActivity.this, options);
-
-            }
-        });
-
-        binding.shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        try {
+//            serverURL = new URL("https://meet.jit.si");
+//            JitsiMeetConferenceOptions defaultOptions =
+//                    new JitsiMeetConferenceOptions.Builder()
+//                            .setServerURL(serverURL)
+//                            .setWelcomePageEnabled(false)
+//                            .build();
+//            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        binding.joinButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+//                        .setRoom(binding.meetingCode.getText().toString())
+//                        .setWelcomePageEnabled(false)
+//                        .build();
+//
+//                JitsiMeetActivity.launch(MainActivity.this, options);
+//
+//            }
+//        });
+//
+//        binding.shareButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch(item.getItemId()){
+
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+
+                case R.id.about:
+                    selectedFragment = new AboutFragment();
+                    break;
+
+            }
+
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/example/viddio/fragments/HomeFragment.java
+++ app/src/main/java/com/example/viddio/fragments/HomeFragment.java
@@ -1,4 +1,76 @@
 package com.example.viddio.fragments;
 
-public class HomeFragment {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.viddio.R;
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HomeFragment extends Fragment {
+
+    private View view;
+    private Button join, share;
+    private EditText meetingCode;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.home_fragment, container, false);
+        try {
+            URL serverURL = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions =
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setServerURL(serverURL)
+                            .setWelcomePageEnabled(false)
+                            .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        widgetSetup();
+
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(meetingCode.getText().toString())
+                        .setWelcomePageEnabled(false)
+                        .build();
+
+                JitsiMeetActivity.launch(requireActivity().getApplicationContext(), options);
+
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+//                sharingIntent.setType("text/plain");
+//                sharingIntent.putExtra(Intent.EXTRA_TEXT, meetingCode.getText().toString());
+//                startActivity(Intent.createChooser(sharingIntent, "Share This Meeting Code Using:"));
+
+            }
+        });
+        return view;
+    }
+
+    private void widgetSetup() {
+        join = (Button)view.findViewById(R.id.joinButton);
+        share = (Button)view.findViewById(R.id.shareButton);
+        meetingCode = (EditText)view.findViewById(R.id.meetingCode);
+    }
 }
